a83383b1938a98e2823e18e0da741daab532fd72
asolano7 <arronsolano@yahoo.com>
2016-07-26 20:50:51 -0400

    Push all items

--- src/MainDriver.java
+++ src/MainDriver.java
@@ -4,7 +4,9 @@ public class MainDriver {
 	public static void main(String[] args) 
 	{
 		// TODO Auto-generated method stub
-		System.out.println("Hello World");
+		System.out.println("What would you like to do? ");
+		System.out.println("Debit = 1");
+		System.out.println("Credit = 1");
 
 	}
 
