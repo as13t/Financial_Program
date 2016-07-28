8cc97012dc1ff0c6093f085dde93f38cf0db3cfb
asolano7 <arronsolano@yahoo.com>
2016-07-27 21:35:34 -0400

    Added credit sum and some credit questions

--- src/Credit.java
+++ src/Credit.java
@@ -1,5 +1,33 @@
 
 public class Credit 
 {
-
+	double creditAmt;
+	double balAmt;
+	double sumBal;
+	
+	public Credit(double creditAmt)
+	{
+		this.creditAmt = creditAmt;
+	}
+	
+	public void setCreditAmt(double creditAmt)
+	{
+		this.creditAmt = creditAmt;
+	}
+	
+	public void setBalanceAmt(double balAmt)
+	{
+		this.balAmt = balAmt;
+	}
+	
+	public double sumItems()
+	{
+		sumBal = this.creditAmt + this.balAmt;
+		return sumBal;
+	}
+	
+	public double getSumAll()
+	{
+		return sumItems();
+	}
 }
--- src/MainDriver.java
+++ src/MainDriver.java
@@ -1,13 +1,40 @@
+import java.util.*;
 
 public class MainDriver {
 
+	@SuppressWarnings("resource")
 	public static void main(String[] args) 
 	{
-		// TODO Auto-generated method stub
-		System.out.println("What would you like to do? ");
-		System.out.println("Debit = 1");
-		System.out.println("Credit = 1");
+		int itemSelect;
+		double creditAmt = 0.00;
+		double debitAmt;
+		double balAmt;
+		
+		Credit creditApp = new Credit(creditAmt);
+		Debit debitApp = new Debit();
 
+		System.out.println("Select one of the below.");
+		System.out.println("Credit = 1");
+		System.out.println("Debit = 2");
+		
+		Scanner input = new Scanner(System.in);
+		Scanner creditInput = new Scanner(System.in);
+		Scanner debitInput = new Scanner(System.in);
+		Scanner balInput = new Scanner(System.in);
+		itemSelect = input.nextInt();
+		
+		if(itemSelect == 1)
+		{
+			System.out.println("What is your balance: ");
+			balAmt = balInput.nextDouble();
+			System.out.println("Please enter Credit Amt: ");
+			creditAmt = creditInput.nextDouble();
+			
+			creditApp.setBalanceAmt(balAmt);
+			creditApp.setCreditAmt(creditAmt);
+			
+			System.out.printf("%s %f", "Your new balance is: ", creditApp.sumItems());
+		}
 	}
 
 }
